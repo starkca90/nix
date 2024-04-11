716498f2465df8e991ae8f4f22b0cb4ab0c4365e
Casey Stark <stark@caseystark.com>
2024-04-11 18:52:08 -0500

    Add Helm

--- user/applications/devops/kubernetes.nix
+++ user/applications/devops/kubernetes.nix
@@ -3,6 +3,7 @@
 {
   home.packages = ( with pkgs; [
     kubectl
+    (wrapHelm kubernetes-helm { plugins = [ kubernetes-helmPlugins.helm-secrets kubernetes-helmPlugins.helm-diff ]; })
     k9s
     lens
   ]);
